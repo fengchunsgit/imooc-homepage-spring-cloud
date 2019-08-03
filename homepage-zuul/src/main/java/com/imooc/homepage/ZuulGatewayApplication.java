031844d9789065efed9c59408515b9b4e9bf5d3c
fengchunsgit <1055688803@qq.com>
2019-08-03 20:11:39 +0800

    gateway

---
+++ homepage-zuul/src/main/java/com/imooc/homepage/ZuulGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.imooc.homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//网关应用程序
+//EnableZuulProxy  标识当前应用是zuulserver
+//SpringCloudApplication  用于简化配置的注解
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class,args);
+    }
+}
